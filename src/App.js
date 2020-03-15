0521b3646bf51fb0d6d5c49b2e29ea8e3deef7be
asadnaeem1 <asadnaeem12345@gmail.com>
2020-03-15 22:30:50 +0500

    react app

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          AWS React Deployment Practice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Asad Naeem</p>
       </header>
     </div>
   );
